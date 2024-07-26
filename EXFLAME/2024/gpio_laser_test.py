d96b337e064ca1e29b2cbc1db39068895eae64ed
Hayden <hmoxsom@gmail.com>
2024-07-26 17:28:24 +1200

    jetson gpio laser testing

---
+++ EXFLAME/2024/gpio_laser_test.py
@@ -0,0 +1,23 @@
+import Jetson.GPIO as GPIO
+import time
+
+# Disable warnings
+GPIO.setwarnings(False)
+
+# Pin Definitions
+led_pin = 12  # BCM pin 18, BOARD pin 12
+
+# Set up the GPIO channel
+GPIO.setmode(GPIO.BOARD)  # Use BOARD pin numbering
+GPIO.setup(led_pin, GPIO.OUT, initial=GPIO.LOW)  # Set pin as output and initialize to low
+
+try:
+    while True:
+        print("high")
+        GPIO.output(led_pin, GPIO.HIGH)  # Turn LED on
+        time.sleep(5)  # Wait for 5 seconds
+        print("low")
+        GPIO.output(led_pin, GPIO.LOW)  # Turn LED off
+        time.sleep(5)  # Wait for 5 seconds
+finally:
+    GPIO.cleanup()  # Clean up GPIO on exit
